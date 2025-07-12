f3a5524be5af1611362ef116eaed758ba572177e
sanki92 <sankalpt92@gmail.com>
2025-07-12 12:53:00 +0530

    feat: create a User collection schema

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select:false
+  },
+  points: {
+    type: Number,
+    default: 10,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  listedItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+    },
+  ],
+  completedSwaps: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "SwapRequest" },
+  ],
+});
+module.exports = mongoose.model("User", userSchema);
