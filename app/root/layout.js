9c0c8780444955b7a36eabc71cdf7495b6437ddd
Shresthap21 <pandeyshrestha2111@gmail.com>
2025-07-12 14:06:19 +0530

    feat: implement new homepage layout with responsive design and add new collection slider

--- app/root/layout.js
+++ app/root/layout.js
@@ -1,8 +1,4 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
